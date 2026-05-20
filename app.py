bceef18297fe47a28e22fc64dfd94d1de0a07701
abdulshamsi555-lgtm <abdulshamsi555@gmail.com>
2026-05-20 16:22:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, send_from_directory
+
+app  =  Flask(__name__)
+
+@app.route('/google1234567890abcd.html')
+def  google_verify():
+    return send_from_directory('.', 'google1234567890abcd.html' )
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        users.append({
+            "name": name,
+            "email": email,
+            "password": password
+        })
+
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        for user in users:
+            if user["email"] == email and user["password"] == password:
+                return redirect(url_for("product"))
+
+        return "Wrong email or password"
+
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
